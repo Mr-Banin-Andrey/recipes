b98424358249a725c9ac6086712a195740ec5bbb
Andrey Banin <mr.banin.andrey@gmail.com>
2025-02-23 11:14:55 +0300

    Initial Commit

---
+++ Recipes/RecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipesApp.swift
+//  Recipes
+//
+//  Created by Андрей Банин on 23.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct RecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
