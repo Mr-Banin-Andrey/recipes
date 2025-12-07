d9720c715defe2752e96d12ee3f854c19b254891
Andrey Banin <>
2025-12-07 23:40:20 +0300

    weekview fixed

--- Recipes/Flows/Main/Stores/MainStore.swift
+++ Recipes/Flows/Main/Stores/MainStore.swift
@@ -10,6 +10,11 @@ import Combine
 
 final class MainStore: ObservableObject {
     
+    // Calendar
+    @Published var weeks: [DayModel] = []
+    @Published var selectedDate: Date = DateConverter.dateOnly(Date())
+    private var currentDate: Date = Date()
+    
     @Published var recipes: [Recipe] = Recipe.fourRecipe
     private var dishLists: [DishList] = []
     private var today: Date = DateConverter.dateOnly(Date())
@@ -35,9 +40,12 @@ final class MainStore: ObservableObject {
     @Published var currentDiningTime: DiningTime?
     @Published var currentMeal: MenuSectionType = .breakfasts
     
-    private var cancellables: Set<AnyCancellable> = []
+//    private var cancellables: Set<AnyCancellable> = []
     
     init() {
+        /// Данные даты не хранятся, каждый раз заново инициализируются
+        fetchCurrentWeek()
+        fetchPreviousNextWeek()
         //        stateKeeper.$recipes
         //            .sink { recipes in
         //                self.recipes = recipes
@@ -72,15 +80,15 @@ final class MainStore: ObservableObject {
         //            .store(in: &cancellables)
     }
     
-    private func currentDate() {
-        let date = Date()
-        var calendar = Calendar.current
-        calendar.timeZone = .current
-        let dateComponents = calendar.dateComponents([.year, .month, .day], from: date)
-        if let dateOnly = calendar.date(from: dateComponents) {
-            today = dateOnly
-        }
-    }
+//    private func currentDate() {
+//        let date = Date()
+//        var calendar = Calendar.current
+//        calendar.timeZone = .current
+//        let dateComponents = calendar.dateComponents([.year, .month, .day], from: date)
+//        if let dateOnly = calendar.date(from: dateComponents) {
+//            today = dateOnly
+//        }
+//    }
         
     /// Ищем по массиву текущий день
     /// и кладём для отображения на экране в mealTime
@@ -106,5 +114,43 @@ final class MainStore: ObservableObject {
 
         dishLists.append(dishListDay)
         dishListForSelectedDay = dishListDay
+    }
+    
+    // Calendar
+    private func isToday(date: Date) -> Bool {
+        let calendar = Calendar.current
+        return calendar.isDate(currentDate, inSameDayAs: date)
+    }
+    
+    /// Создаёт и добавляет текущую неделю в массив
+    private func fetchCurrentWeek() {
+        var calendar = Calendar.current
+        calendar.firstWeekday = 2
+        
+        guard let startOfWeek = calendar.date(from: calendar.dateComponents([ .yearForWeekOfYear, .weekOfYear], from: currentDate)) else { return }
+        
+        (0..<7).forEach { day in
+            if let weekday = calendar.date(byAdding: .day, value: day, to: startOfWeek) {
+                weeks.append(DayModel(id: UUID().uuidString, date: weekday))
+                //                currentWeek.append(weekday)
+            }
+        }
+    }
+    
+    /// Создаёт и добавляет следующую неделю в массив
+    private func fetchPreviousNextWeek() {
+        guard let nextWeekToday = Calendar.current.date(byAdding: .day, value: 7, to: currentDate) else { return }
+        
+        var calendar = Calendar.current
+        calendar.firstWeekday = 2
+        
+        guard let startOfWeekNext = calendar.date(from: calendar.dateComponents([.yearForWeekOfYear, .weekOfYear], from: nextWeekToday)) else { return }
+        
+        (0..<7).forEach{ day in
+            if let weekday = calendar.date(byAdding: .day, value: day, to: startOfWeekNext) {
+                weeks.append(DayModel(id: UUID().uuidString, date: weekday))
+                //                currentWeek.append(weekday)
+            }
+        }
     }
 }
--- Recipes/Flows/Main/Views/WeekView.swift
+++ Recipes/Flows/Main/Views/WeekView.swift
@@ -9,24 +9,14 @@ import SwiftUI
 
 struct WeekView: View {
     
-    private var currentDate: Date = Date()
-    @State private var selectedDate: Date = DateConverter.dateOnly(Date())
-    @State private var weeks: [DayModel] = []
-    private let onDayTap: (Date) -> Void
+    @EnvironmentObject private var store: MainStore
+    var onDayTap: (Date) -> Void
 
-    init(onDayTap: @escaping (Date) -> Void) {
-        self.onDayTap = onDayTap
-        
-        /// Данные даты не хранятся, каждый раз заново инициализируются
-        self.fetchCurrentWeek()
-        self.fetchPreviousNextWeek()
-    }
-    
     var body: some View {
         GeometryReader { geometry in
             ScrollView(.horizontal, showsIndicators: false) {
                 HStack(alignment: .center, spacing: 0) {
-                    ForEach(weeks) { day in
+                    ForEach(store.weeks) { day in
                         VStack {
                             Text(convertDateToString(
                                 date: day.date,
@@ -43,7 +33,7 @@ struct WeekView: View {
                             .font(.system(size: 18, weight: .regular))
                             .foregroundStyle(.mainText)
                             .padding(10)
-                            .background(selectedDate == DateConverter.dateOnly(day.date)
+                            .background(store.selectedDate == DateConverter.dateOnly(day.date)
                                         ? .frostFairy
                                         : .clear)
                             .clipShape(.rect(cornerRadius: 100))
@@ -53,7 +43,7 @@ struct WeekView: View {
                             height: 85
                         )
                         .onTapGesture {
-                            self.selectedDate = DateConverter.dateOnly(day.date)
+                            store.selectedDate = DateConverter.dateOnly(day.date)
                             self.onDayTap(day.date)
                         }
                     }
@@ -69,43 +59,6 @@ struct WeekView: View {
         formatter.dateFormat = format
         formatter.locale = Locale(identifier: "ru_RU")
         return formatter.string(from: date)
-    }
-    
-    private func isToday(date: Date) -> Bool {
-        let calendar = Calendar.current
-        return calendar.isDate(currentDate, inSameDayAs: date)
-    }
-    
-    /// Создаёт и добавляет текущую неделю в массив
-    private func fetchCurrentWeek() {
-        var calendar = Calendar.current
-        calendar.firstWeekday = 2
-        
-        guard let startOfWeek = calendar.date(from: calendar.dateComponents([ .yearForWeekOfYear, .weekOfYear], from: currentDate)) else { return }
-        
-        (0..<7).forEach { day in
-            if let weekday = calendar.date(byAdding: .day, value: day, to: startOfWeek) {
-                weeks.append(DayModel(id: UUID().uuidString, date: weekday))
-                //                currentWeek.append(weekday)
-            }
-        }
-    }
-    
-    /// Создаёт и добавляет следующую неделю в массив
-    private func fetchPreviousNextWeek() {
-        guard let nextWeekToday = Calendar.current.date(byAdding: .day, value: 7, to: currentDate) else { return }
-        
-        var calendar = Calendar.current
-        calendar.firstWeekday = 2
-        
-        guard let startOfWeekNext = calendar.date(from: calendar.dateComponents([.yearForWeekOfYear, .weekOfYear], from: nextWeekToday)) else { return }
-        
-        (0..<7).forEach{ day in
-            if let weekday = calendar.date(byAdding: .day, value: day, to: startOfWeekNext) {
-                weeks.append(DayModel(id: UUID().uuidString, date: weekday))
-                //                currentWeek.append(weekday)
-            }
-        }
     }
 }
 
